c30ac1251769eda5dd76774dd0f43bdbbce0ab0b
lizasha228-max <lizasha228@gmail.com>
2025-09-24 17:36:31 +0300

    add repo

---
+++ script.js
@@ -0,0 +1,36 @@
+// const defoultAlert = () => {
+// const iconId = document.querySelector(".fa-regular far");
+// iconId.style.display = "none";
+// }
+
+// function changeColor(inmputEle){
+//   console.log(inmputEle);
+// }
+
+// const objLiza = {
+//   animans: 'cat',
+//   catAnim: true
+// };
+
+// const catObj = localStorage.getItem("dataCat");
+// console.log("kmkmkmk",catObj);
+// const parsObj = JSON.parse(catObj);
+// console.log(parsObj);
+
+// const generalObj = [
+//   { name: 'Luna', animal: true },
+//   { name: 'Dog', animal: false },
+//   { titlesHeadersInfo: [ 'Airlines', 'Department', 'Duration', 'lines' ] }
+// ];
+
+
+//  localStorage.setItem("tittlesHeader",JSON.stringify(generalObj[2]));
+
+//   const getObj = () => {
+//    const tittleObj = localStorage.getItem("tittlesHeader");
+//     const tittleArr = JSON.parse(tittleObj);
+//     const x = tittleArr.titlesHeadersInfo;
+//     console.log(x);
+//  }
+ 
+
